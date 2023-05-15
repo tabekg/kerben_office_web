caf13d8d5e94104bc19da8aa32959e231be0a991
Bekbolot Tazhibaev <tabekg@gmail.com>
2023-05-15 10:07:34 +0600

    fix

--- src/containers/AppContainer.jsx
+++ src/containers/AppContainer.jsx
@@ -1,13 +1,42 @@
-import HomeContainer from './HomeContainer.jsx'
-import {Container, Nav, Navbar} from 'react-bootstrap'
+﻿import HomeContainer from './HomeContainer.jsx'
+import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap'
 import {useContext, useState} from 'react'
 import {RootContext} from '../utils/context.js'
 import ChangePasswordModalComponent from '../components/ChangePasswordModalComponent.jsx'
+import {Button, Modal} from 'react-bootstrap'
+import {MdDeleteForever} from 'react-icons/md'
+import {GrEdit} from 'react-icons/gr'
+import {LANGUAGES} from '../utils/config.js'
+import {useTranslation} from 'react-i18next'
 
 export default function AppContainer() {
   const root = useContext(RootContext)
+  const {t} = useTranslation()
 
   const [changePasswordModal, setChangePasswordModal] = useState(false)
+
+  const [showModal, setShowModal] = useState(false)
+
+  const handleButtonClick = () => {
+    setShowModal(true)
+  }
+
+  const handleCloseModal = () => {
+    setShowModal(false)
+  }
+
+  const [blocks, setBlocks] = useState([])
+
+  const addBlock = () => {
+    const newBlock = {
+      id: Date.now(), // Уникальный идентификатор блока
+    }
+    setBlocks([...blocks, newBlock])
+  }
+  const handleDeleteBlock = (id) => {
+    const updatedBlocks = blocks.filter((block) => block.id !== id)
+    setBlocks(updatedBlocks)
+  }
 
   return (
     <>
@@ -19,35 +48,79 @@ export default function AppContainer() {
       <div style={{display: 'flex', flexDirection: 'column', height: '100%'}}>
         <Navbar bg='dark' expand='lg' variant={'dark'}>
           <Container>
-            <Navbar.Brand href='/'>Панель управления Кербен</Navbar.Brand>
+            <Navbar.Brand href='/'>{t('kerben_control_panel')}</Navbar.Brand>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id='basic-navbar-nav'>
               <Nav className='ms-auto'>
+                <Nav.Link onClick={handleButtonClick}>Оператор</Nav.Link>
+                <NavDropdown
+                  title={
+                    LANGUAGES.find((g) => g.code === root.language)?.title ||
+                    'Сменить язык'
+                  }
+                  id='change-language-dropdown'
+                >
+                  {LANGUAGES.map((g, i) => (
+                    <NavDropdown.Item
+                      key={i}
+                      href='#'
+                      onClick={() => root.setLanguage(g.code)}
+                    >
+                      {g.title}
+                    </NavDropdown.Item>
+                  ))}
+                </NavDropdown>
                 <Nav.Link href='#' onClick={() => setChangePasswordModal(true)}>
-                  Сменить пароль
+                  {t('change_password')}
                 </Nav.Link>
                 <Nav.Link href='#' onClick={() => root.signOut()}>
-                  Выйти
+                  {t('sign_out')}
                 </Nav.Link>
-                {/*<NavDropdown title='Dropdown' id='basic-nav-dropdown'>*/}
-                {/*  <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>*/}
-                {/*  <NavDropdown.Item href='#action/3.2'>*/}
-                {/*    Another action*/}
-                {/*  </NavDropdown.Item>*/}
-                {/*  <NavDropdown.Item href='#action/3.3'>*/}
-                {/*    Something*/}
-                {/*  </NavDropdown.Item>*/}
-                {/*  <NavDropdown.Divider />*/}
-                {/*  <NavDropdown.Item href='#action/3.4'>*/}
-                {/*    Separated link*/}
-                {/*  </NavDropdown.Item>*/}
-                {/*</NavDropdown>*/}
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
         <HomeContainer />
       </div>
+
+      <Modal show={showModal} onHide={handleCloseModal} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Операторы</Modal.Title>
+        </Modal.Header>
+        <div className='oper-conteiner'>
+          {blocks.map((block, index) => (
+            <Modal.Body key={index} className='oper-name'>
+              <p style={{fontSize: '20px'}}>Оператордун аты жону</p>
+              <div>
+                <GrEdit
+                  style={{
+                    fontSize: '25px',
+                    cursor: 'pointer',
+                    marginLeft: '20px',
+                  }}
+                />
+                <MdDeleteForever
+                  onClick={() => handleDeleteBlock(block.id)}
+                  style={{
+                    fontSize: '30px',
+                    cursor: 'pointer',
+                    color: 'red',
+                    marginLeft: '20px',
+                  }}
+                />
+              </div>
+            </Modal.Body>
+          ))}
+        </div>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleCloseModal}>
+            Закрыть
+          </Button>
+          <Button variant='primary' onClick={addBlock}>
+            Оператор кошуу
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   )
 }
