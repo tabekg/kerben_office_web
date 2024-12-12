4f63461f2ca9d69c55f08f96907c4f3b81d33cac
Bekbolot Tazhibaev <tabekg@gmail.com>
2024-12-12 17:59:56 +0600

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # cd /var/www/kerben/office-web && git pull && yarn install && yarn build
 
 yarn build
-ssh besoft "rm -rf /var/www/kerben/office-web/dist"
-scp -r ./dist besoft:/var/www/kerben/office-web
+ssh besoftkg "rm -rf /var/www/kerben/office-web/dist"
+scp -r ./dist besoftkg:/var/www/kerben/office-web
